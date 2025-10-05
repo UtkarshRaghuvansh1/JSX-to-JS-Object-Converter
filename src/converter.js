76af8a45b9e79936efb545a6b4b6d57cf30f94dd
Utkarsh Ragghuvanshi <utkarshragghuvanshi@Utkarshs-MacBook-Air.local>
2025-10-05 11:00:18 +0530

    starter template for converter.js

---
+++ src/converter.js
@@ -0,0 +1,9 @@
+//Import Babel library
+import * as Babel from "@babel/standalone";
+
+// This will allow me to run babel inside the browser
+// No build setup needed
+
+// Function to Convert JSX to JS
+// JSX will be in form of string
+export function convertJSXtoObject(code) {}
